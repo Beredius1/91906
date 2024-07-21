0b1a20e7eaed544116c6d637125e9e2b21fd2967
Mr Wright <118939094+Beredius1@users.noreply.github.com>
2024-07-22 11:57:00 +1200

    Update Version_2.py
    My first concept, I have completed my initial comments, my list, created a submit, completed the size of the window and I made resizable, created an entry box too.

--- Version_1.py
+++ Version_1.py
@@ -1,2 +1,24 @@
-print("hello world")
-x = input("Hello, what is your name? ")
+'''Purpose: This is a program that will upskill the mathmatical abilities of
+pre-schoolers.
+Name: Mr Wright
+Date: 22 July 2024
+'''
+
+
+import random
+import tkinter import*
+
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9 }
+
+app = Tk()
+app.title("Math4Kids")
+app.geometry("800x600")
+app.resizable(True, True)
+
+start = Button(app, text = "Start")
+start.place()
+
+solving = Entry(app)
+solving.place()
+
+submit = Button(app, text ='Submit')
